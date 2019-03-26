a575dc19ca2713f75772b78236e6b47d41470f80
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2019-03-26 12:47:26 -0600

    updates for cmake variables

--- arm.cmake
+++ arm.cmake
@@ -7,14 +7,14 @@ file(GLOB_RECURSE HEADER_FILES ${CMAKE_SOURCE_DIR}/include/* ${CMAKE_SOURCE_DIR}
 
 set(SOURCES_PREFIX ${CMAKE_SOURCE_DIR}/src)
 add_subdirectory(src)
-set(SOS_LIB_SOURCELIST ${SOURCES} ${HEADER_FILES})
+set(SOS_SOURCELIST ${SOURCES} ${HEADER_FILES})
 
 #lwip kernel
-set(SOS_LIB_TYPE release)
-set(SOS_LIB_OPTION kernel)
-set(SOS_LIB_INCLUDE_DIRECTORIES include ${LWIP_NAME}/src/include)
+set(SOS_CONFIG release)
+set(SOS_OPTION kernel)
+set(SOS_INCLUDE_DIRECTORIES include ${LWIP_NAME}/src/include)
 include(${SOS_TOOLCHAIN_CMAKE_PATH}/sos-lib-std.cmake)
 
-set(SOS_LIB_TYPE debug)
-set(SOS_LIB_INCLUDE_DIRECTORIES include ${LWIP_NAME}/src/include)
+set(SOS_CONFIG debug)
+set(SOS_INCLUDE_DIRECTORIES include ${LWIP_NAME}/src/include)
 include(${SOS_TOOLCHAIN_CMAKE_PATH}/sos-lib-std.cmake)
